ab1fa549cb707de0f6b3c3a9c6f9e2d68d033974
Imran <imranr2@msn.com>
2023-09-27 11:51:22 +0800

    Add script to call PAT and update mdp pred csv probabilities

--- Multi_PCSP_Generator.py
+++ Multi_PCSP_Generator.py
@@ -1,16 +1,17 @@
 from csv import DictReader
-from datetime import date, timedelta
-
+import datetime
 from Generate_PCSP import generate_pcsp
 
 with open('MDP_pred.csv', newline='') as pred_file:
     pred_reader = DictReader(pred_file)
     row_num = 1
     for row in pred_reader:
-        end_date = date.fromisoformat(row['date']) - timedelta(days=1)
+        print(row['date'])
+        date_obj = datetime.datetime.strptime(row['date'], '%Y-%m-%d')
+        end_date = date_obj - datetime.timedelta(days=1)
         pcsp_filename = f"{row_num}.pcsp"
         generate_pcsp(
-            end_date.isoformat(), row['P1Name'], row['P2Name'], row['P1Hand'], row['P2Hand'], pcsp_filename
+            end_date.strftime('%Y-%m-%d'), row['P1Name'], row['P2Name'], row['P1Hand'], row['P2Hand'], "./pcsp_files/" + pcsp_filename
         )
         print(f"Generated {pcsp_filename}")
         row_num += 1
