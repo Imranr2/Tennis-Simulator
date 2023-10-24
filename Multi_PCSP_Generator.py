91fbc04099b88e7e7778ab19efc229bf4722189f
sivayogasubramanian <sivayogasubramanian@gmail.com>
2023-10-24 19:41:58 +0800

    Final update

--- Multi_PCSP_Generator.py
+++ Multi_PCSP_Generator.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 import datetime
 from Generate_PCSP_approach import generate_pcsp_approach
+from Generate_PCSP_baseline import generate_pcsp_baseline
 from Generate_PCSP_depth import generate_pcsp_depth
 from Generate_PCSP_extra_shots import generate_pcsp_extra_shots
 from Generate_PCSP_prev_shots import generate_pcsp_prev_shots
@@ -10,7 +11,7 @@ import sys
 start_line, end_line = int(sys.argv[1]), int(sys.argv[2])
 print(start_line, end_line)
 
-with open('MDP_pred.csv', newline='') as pred_file:
+with open('MDP_pred_BASELINE.csv', newline='') as pred_file:
     data = read_data_from_csv()
     pred_reader = DictReader(pred_file)
 
@@ -39,4 +40,7 @@ with open('MDP_pred.csv', newline='') as pred_file:
                 row['P1Hand'], row['P2Hand'], "./pcsp_files/" + f"{i + 1}_PREV_SHOTS.pcsp"
             )
             
-            print(f"Generated {i + 1}_APPROACH.pcsp, Generated {i + 1}_DEPTH.pcsp, {i + 1}_EXTRA_SHOTS.pcsp, {i + 1}_PREV_SHOTS.pcsp")
+            generate_pcsp_baseline(
+                data, end_date.strftime('%Y-%m-%d'), row['P1Name'], row['P2Name'], 
+                row['P1Hand'], row['P2Hand'], "./pcsp_files/" + f"{i + 1}_BASELINE.pcsp"
+            )
--- Update_Probabilites.py
+++ Update_Probabilites.py
@@ -5,36 +5,38 @@ import csv
 
 def callPAT(patPath, count):
   patOutputFile =  "./result.txt"
-  mdpPredFile = "./MDP_pred.csv"
+  
+  for model in ["BASELINE", "DEPTH", "EXTRA_SHOTS", "PREV_SHOTS", "BASELINE"]:
+    mdpPredFile = f"./MDP_pred_{model}.csv"
 
-  f = open(patOutputFile, "w")
-  f.close()
-  mdpPredLines = [line for line in csv.reader(open(mdpPredFile, 'r'))]
-  newMdpPredFp = open("./MDP_pred_new.csv", 'w')
-  newMdpPred = csv.writer(newMdpPredFp)
-  header = ["date","P1Name","P2Name","P1WinProb","P2WinProb","P1Hand","P2Hand","Gender"]
-  newMdpPred.writerow(header)
-  for i in range(1, count + 1):
-    args = [patPath + "/PAT3.Console.exe", "-pcsp", f"./pcsp_files/{i}.pcsp", os.getcwd() + "/result.txt"]
-    subprocess.call(args)
-    
-    p1Win = 0
-    p2Win = 0
-    fp = open(patOutputFile, 'r')
-    for j, line in enumerate(fp):
-      if j == 3:
-        if "NOT valid" in line:
-          continue
-        probability = line.split("Probability")[1].strip()
-        lowerBound, upperBound = probability[1:-2].split(',')
-        p1Win = (float(lowerBound) + float(upperBound)) / 2
-        p2Win = 1 - p1Win
-    
-    curLine = mdpPredLines[i]
-    curLine[3], curLine[4] = p1Win, p2Win
-    if p1Win == 0 or p2Win == 0:
-      continue
-    newMdpPred.writerow(curLine)
+    f = open(patOutputFile, "w")
+    f.close()
+    mdpPredLines = [line for line in csv.reader(open(mdpPredFile, 'r'))]
+    newMdpPredFp = open(f"./MDP_pred_new_{model}.csv", 'w')
+    newMdpPred = csv.writer(newMdpPredFp)
+    header = ["date","P1Name","P2Name","P1WinProb","P2WinProb","P1Hand","P2Hand","Gender"]
+    newMdpPred.writerow(header)
+    for i in range(1, count + 1):
+      args = [patPath + "/PAT3.Console.exe", "-pcsp", f"./pcsp_files/{i}_{model}.pcsp", os.getcwd() + "/result.txt"]
+      subprocess.call(args)
+      
+      p1Win = 0
+      p2Win = 0
+      fp = open(patOutputFile, 'r')
+      for j, line in enumerate(fp):
+        if j == 3:
+          if "NOT valid" in line:
+            continue
+          probability = line.split("Probability")[1].strip()
+          lowerBound, upperBound = probability[1:-2].split(',')
+          p1Win = (float(lowerBound) + float(upperBound)) / 2
+          p2Win = 1 - p1Win
+      
+      curLine = mdpPredLines[i]
+      curLine[3], curLine[4] = p1Win, p2Win
+      if p1Win == 0 or p2Win == 0:
+        continue
+      newMdpPred.writerow(curLine)
 
 args = sys.argv
 if len(args) != 3:
